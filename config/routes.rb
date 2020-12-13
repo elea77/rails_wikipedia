b54f3af95a250ae19d82d9fc41b9625ecda4782f
Eléa Carton <eleacarton@MacBook-Air.local>
2020-12-13 12:01:32 +0100

    Delete username form edit + Modif routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get  '/sign_in', to: 'sessions#sign_in_form'
+  get '/sign_in', to: 'sessions#sign_in_form'
   post '/sign_in', to: 'sessions#sign_in'
   delete '/sign_out', to: 'sessions#sign_out'
+
+  get '/sign_up', to: 'users#new'
 
 
 end
